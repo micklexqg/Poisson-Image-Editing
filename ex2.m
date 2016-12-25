bd36944cac6eacec54469b63b58f93f344c29e16
JulienSiems <juliensiems@gmail.com>
2016-12-25 22:44:52 +0000

    Part 3 sort of works

--- ex2.m
+++ ex2.m
@@ -7,51 +7,45 @@ bwi = 1 - bw;
 f = I - bwi;
 f(f < 0) = 0;
 % bulding xima
-[f_row, f_col, f_val] = find(f);
+[bw_row, bw_col, ~] = find(bw);
+linindx = sub2ind(size(I), bw_row, bw_col);
 
 %% building b
-fstar = I-bw;
-fstar(fstar < 0) = 0;
-
-fstar_p = padarray(fstar, [1,1], 'symmetric');
-
-% 4 neighborhood
-filter = [0 1 0; 1 0 1; 0 1 0];
-sum_fstar = imfilter(fstar, filter, 'replicate');
-sum_fstar_max = max(max(sum_fstar));
-nsum_fstar = sum_fstar/sum_fstar_max;
-
-% determining the pixels corresponding to the border.
-% all the pixel at the order have a non zero value
-% Let's get them.
-
-h = nsum_fstar - bwi;
-h(h < 0) = 0;
-h = h * sum_fstar_max;
-
-[fstar_row, fstar_col, fstar_val] = find(h+bw);
-% normalize back
-fstar_val = fstar_val - 1;
-b = fstar_val;
 
 % guidance field v_pq = g_p - g_q
-% I_p = padarray(I, [1,1], 'symmetric');
-% diffI_t = I_p(2:end-1, 2:end-1) - I_p(2:end-1, 1:end-2);
-% diffI_r = I_p(2:end-1, 2:end-1) - I_p(3:end, 2:end-1);
-% diffI_b = I_p(2:end-1, 2:end-1) - I_p(2:end-1, 3:end);
-% diffI_l = I_p(2:end-1, 2:end-1) - I_p(1:end-2, 2:end-1);
-% 
-% sum_v_neighbours = diffI_t + diffI_r + diffI_b + diffI_l;
 filter = [0 -1 0; -1 4 -1; 0 -1 0];
 sum_v_neighbours = imfilter(double(I), filter, 'replicate');
-
 % extract domain
-h3 = sum_v_neighbours - bwi;
+h3 = sum_v_neighbours - bwi*4;
 h3(h3 < 0) = 0;
 
 [v_row, v_col, v_val] = find(h3+bw);
 v_val = v_val - 1;
-% b = b + v_val;
+
+linindx = sub2ind(size(I), v_row, v_col);
+I(linindx) = v_val;
+
+
+fstar = I - bw;
+fstar(fstar < 0) = 0;
+
+% 4 neighborhood
+filter = [0 1 0; 1 0 1; 0 1 0];
+sum_fstar = imfilter(fstar, filter, 'replicate');
+
+% determining the pixels corresponding to the border.
+% all the pixel at the order have a non zero value
+% Let's get them.
+h = sum_fstar - bwi*4;
+h(h < 0) = 0;
+[fstar_row, fstar_col, fstar_val] = find(h+bw);
+% normalize back
+fstar_val = fstar_val - 1;
+
+b = fstar_val;
+
+
+% b = fstar_val + v_val;
 
 
 %% COMPUTING A
@@ -64,12 +58,12 @@ h2 = (sum_N - bwi)*N_max;
 h2(h2 < 0) = 0;
 [N_row, N_col, N_val] = find(h2);
 
-% Ugly for loop for a's
-dim = size(f_col, 1);
+% Ugly for loop for adjacency
+dim = size(bw_col, 1);
 A = zeros(dim);
 [w, l, c] = size(I);
 
-coor = [f_row, f_col];
+coor = [bw_row, bw_col];
 
 % THIS IS WRONG
 for x = 1:w
@@ -98,7 +92,7 @@ end
 
 A = A + diag(ones(1,dim)*4);
 % A = A + diag(N_val);
-linindx = sub2ind(size(I), f_row, f_col);
+linindx = sub2ind(size(I), bw_row, bw_col);
 x = sparse(A)\b;
 I(linindx) = x;
 
